e63b4d318f543b550a47edb0ee7c50e32c2c58d3
Calvin <calvinquach@gmail.com>
2017-12-07 22:23:44 -0500

    Draw optical flow on video.

--- OpenCV3_Template/main.cpp
+++ OpenCV3_Template/main.cpp
@@ -1,9 +1,73 @@
-#include "opencv2\opencv.hpp"
+#include <opencv2/opencv.hpp>
+#include <iostream>
 
 using namespace cv;
+using namespace std;
 
-int main(int argv, char** argc) {
-	Mat test = imread("test.jpg", CV_LOAD_IMAGE_UNCHANGED);
-	imshow("test",test);
-	waitKey();
+int main(int, char**)
+{
+	//Open the first webcam plugged in the computer
+	VideoCapture camera(0);
+	if (!camera.isOpened())
+	{
+		cerr << "ERROR: Could not open camera" << endl;
+		return 1;
+	}
+
+	//Create a window to display the images from the webcam
+	namedWindow("Webcam", CV_WINDOW_AUTOSIZE);
+
+	//number of frames til reseting opticalflow feature markers
+	int FRAME_COUNTER = 0; 
+
+	Mat current_frame;
+	Mat old_frame;
+
+	Mat old_gray;
+	Mat current_gray;
+	
+	//points on prev frame and current frame
+	vector<Point2f> points[2];
+
+	//Read first frame
+	camera >> old_frame;
+	cvtColor(old_frame, old_gray, COLOR_BGR2GRAY);
+	goodFeaturesToTrack(old_gray, points[0], 500, .01, 10, Mat(), 3, 3, 0, 0.04);
+	while (1)
+	{
+		FRAME_COUNTER++;
+		vector<uchar> status;
+		vector<float> err;
+
+		//reset good features markers
+		if (FRAME_COUNTER > 20) {
+			camera >> old_frame;
+			cvtColor(old_frame, old_gray, COLOR_BGR2GRAY);
+			goodFeaturesToTrack(old_gray, points[0], 500, .01, 10, Mat(), 3, 3, 0, 0.04);
+			FRAME_COUNTER = 0;
+		}
+
+		//find the features of the new frame
+		camera >> current_frame;
+		cvtColor(current_frame, current_gray, COLOR_BGR2GRAY);
+		goodFeaturesToTrack(current_gray, points[1], 500, .01, 10, Mat(), 3, 3, 0, 0.04);
+
+		//calculate the optical flow from old frame to current frame
+		calcOpticalFlowPyrLK(old_frame, current_frame, points[0], points[1], status, err);
+
+		//draw cicle on features and lines from old frame to current frame
+		for (int i = 0; i < points[1].size(); i++) {
+			line(current_frame, points[1][i], points[0][i], Scalar(0, 255, 0));
+			circle(current_frame, points[1][i], 3, Scalar(0, 255, 0), -1, 8);
+		}
+		//Show the image on the window
+		imshow("Webcam", current_frame);
+
+		//Wait for a key to be pressed
+		if (waitKey(30) >= 0) break;
+	}
+
+	//Success. The program accomplished its mission and now it can go
+	// to the heaven of programs.
+	return 0;
 }
