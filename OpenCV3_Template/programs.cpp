5fa314bdbcf5095865aa733aaa9bfb7bb8c3195b
Calvin <calvinquach@gmail.com>
2017-12-18 11:13:06 -0500

    Add trackbar callback functions.

---
+++ OpenCV3_Template/programs.cpp
@@ -0,0 +1,28 @@
+#include "programs.h"
+
+using namespace std;
+
+
+namespace Programs {
+
+	void on_num_trackers_trackbar(int, void*) {
+
+	}
+
+	void on_num_hue_trackbar(int, void*) {
+
+	}
+
+	void on_num_saturation_trackbar(int, void*) {
+
+	}
+
+	void on_num_value_trackbar(int, void*) {
+
+	}
+
+	void on_delay_trackbar(int, void*) {
+
+	}
+
+}
--- OpenCV3_Template/programs.h
+++ OpenCV3_Template/programs.h
@@ -4,7 +4,15 @@
 
 namespace Programs {
 
+	void on_num_trackers_trackbar(int, void*);
 
+	void on_num_hue_trackbar(int, void*);
+
+	void on_num_saturation_trackbar(int, void*);
+
+	void on_num_value_trackbar(int, void*);
+
+	void on_delay_trackbar(int, void*);
 
 }
 #endif
