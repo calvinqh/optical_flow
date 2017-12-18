fd6556b90ab5ef2013320cac013684fb386ce36a
Calvin <calvinquach@gmail.com>
2017-12-18 14:33:48 -0500

    Remove some imports.

--- OpenCV3_Template/main.cpp
+++ OpenCV3_Template/main.cpp
@@ -1,13 +1,10 @@
 #include "programs.h"
 
 #include <opencv2/opencv.hpp>
-#include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
-#include <opencv2/core/utility.hpp>
-#include "opencv2/video/tracking.hpp"
 #include "opencv2/videoio.hpp"
 #include <iostream>
-#include "opencv2/highgui.hpp"
+
 
 using namespace cv;
 using namespace std;
